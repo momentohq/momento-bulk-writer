bc787b9ffc491ddcc78c4cd2535fbe6c142fcf85
Michael Landis <michael@momentohq.com>
2023-05-11 17:01:44 -0700

    feat: run the rdbcli tool directly instead of on docker
    The rdbcli tool docker container is built on linux/amd64 and runs the
    JVM. I have been unable to run this container on macos, even with the
    appropriate platform argument.
    
    In order to ensure users on different platforms can run the tool, I am
    bundling the tool with the distributable. Per the tool's license,
    Apache 2.0, this is OK as the tool comes with the license.

--- scripts/ec2-user-data.sh
+++ scripts/ec2-user-data.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 yum update -y
 
-# set up docker
-amazon-linux-extras install docker
-systemctl enable docker
-systemctl start docker
-usermod -a -G docker ec2-user
-newgrp docker
-
 # install dotnet runtime
 rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
 yum install -y aspnetcore-runtime-6.0
 yum install -y dotnet-sdk-6.0
+
+# install java (necessary for rdbcli)
+yum install -y java
