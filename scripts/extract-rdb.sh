d238146eb8488905e005cc68e5d129a610e4b71f
pratik151192 <pratik@momentohq.com>
2024-04-22 13:07:09 -0700

    feat: add sorted set support in redis reload (#46)
    * feat: add sorted set support in redis reload
    
    * feat: add sorted set support in redis reload
    
    * chore: fix utf encoding
    
    * chore: address comments
    
    * chore: address comments

--- scripts/extract-rdb.sh
+++ scripts/extract-rdb.sh
@@ -2,9 +2,11 @@
 #set -x
 
 function usage_exit() {
-    echo "Usage: $0 <data_path> <output-path>";
+    echo "Usage: $0 <data_path> <output_path> [rdb_cli_path]";
     echo "  <data_path>   path to a directory with rdb files";
-    echo "  <output-path> path to a directory where the output will be written";
+    echo "  <output_path> path to a directory where the output will be written";
+    echo "  [rdb_cli_path] optional path to the `bin/rct` executable used to run the extraction";
+    echo "   (default: third-party/redis-rdb-cli/bin/rct)";
     echo
     echo "Description: Extracts rdb files to jsonl files using the rct tool.";
     echo "  rdb files are assumed to be located at: <data_path>/*rdb";
@@ -12,11 +14,23 @@ function usage_exit() {
     exit 1;
 }
 
+# Check for minimum required parameters
+if [ "$#" -lt 2 ]; then
+    usage_exit
+fi
+
 # Parse CLI args
-rdb_cli_path="third-party/redis-rdb-cli/bin/rct"
+default_rdb_cli_path="third-party/redis-rdb-cli/bin/rct"
 
 data_path=$1
 output_path=$2
+
+# Check if rdb_cli_path is provided as an argument
+if [ -n "$3" ]; then
+    rdb_cli_path=$3
+else
+    rdb_cli_path=$default_rdb_cli_path
+fi
 
 if [ -z "$data_path" ]; then
   echo "Need to set data_path"
@@ -62,6 +76,7 @@ file_exists_or_panic $rdb_cli_path
 echo "=== EXTRACT RDB WITH THE FOLLOWING SETTINGS ==="
 echo "data_path = ${data_path}"
 echo "output_path = ${output_path}"
+echo "rdb_cli_path = ${rdb_cli_path}"
 
 
 # Flush any data from previous runs
---
+++ tests/Momento.Etl/Model.Tests/RedisSortedSetTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Momento.Etl.Model;
+using Xunit;
+
+namespace Momento.Etl.Model.Tests;
+
+public class RedisSortedSetTest
+{
+    [Fact]
+    public void PayloadSizeInBytes_HasExpiry_HappyPath()
+    {
+        var item = new RedisSortedSet("key", new Dictionary<string, double>() { { "taylor", 1.23 }, { "alexa", 665.12 } }, 123);
+        Assert.Equal(38, item.ItemSizeInBytes());
+    }
+}
